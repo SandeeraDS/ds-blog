f3e2facabad748c7cbe33e2390503eb00c270b93
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-01 10:46:21 +0530

    Step 2 - inline styles

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
-    const title: string = "Welcome to the new blog";
-    const likes:number =50;
-    const googleLink:string = "https://www.google.com/";
     return (
         <div className="App">
+            <Navbar/>
             <div className="content">
-                <h1>{title}</h1>
-                <p>Liked {likes} times</p>
-                <p>{10}</p>
-                <p>{"Hello, there"}</p>
-                <p>{[1,2,3]}</p>
-                <p>{Math.random()*10}</p>
-                <a href={googleLink}>Google Site</a>
+                <Home/>
             </div>
         </div>
     );
