1fc5223047f7f64860ec45f3db98b6512b684b9d
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-01 14:09:00 +0530

    Step 4 - useStateHook

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,19 @@
-const Home = () => {
-    const handleClick = (e)=>{
-        console.log("hello, Sandeera " , e);
-    }
+import {useState} from "react";
 
-    const handleClickAgain = (name,e)=>{
-        // console.log("hello," + name);
-        console.log("My name is " + name,e.target)
+const Home = () => {
+    const [name,setName] = useState('mario');
+    const [age,setAge] = useState(25);
+    const handleClick = (e)=>{
+        setName('Sandeera');
+        setAge(30)
     }
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p> {name}</p>
+            <p>{age}</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e)=>handleClickAgain("Sandeera",e)}>Click Me 2</button>
         </div>
     );
 }
