2a934b36dc23e51554d81334e332f70c8ff3b0e0
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-03 19:58:30 +0530

    Step 12 - fetching error handling

--- src/Home.js
+++ src/Home.js
@@ -4,24 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
 
     const [blogs, setBlogs] = useState(null);
-    const [isPending,setIsPending] = useState(true);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
     useEffect(() => {
         fetch(' http://localhost:8000/blogs').then(
             res => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for thar resource');
+                }
                 return res.json()
             }
         ).then((data) => {
-            console.log(data);
+            setError(null);
             setBlogs(data);
             setIsPending(false);
+        }).catch((err) => {
+            setIsPending(false);
+            setError(err.message);
         })
     }, [])
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {isPending && <div>Loading....</div>}
-            { blogs && <BlogList blogs={blogs} title="All Blogs"/>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
