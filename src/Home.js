0f91ad61ffcbe815c4d483b25de5d925228df20a
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-03 19:02:20 +0530

    Step 10 - fetching data with useEffect (  npx json-server --watch data/db.json --port 8000 )

--- src/Home.js
+++ src/Home.js
@@ -3,29 +3,23 @@ import BlogList from "./BlogList";
 
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1},
-        {title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2},
-        {title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3}
-    ]);
-    const [name,setName] = useState('Mario');
+    const [blogs, setBlogs] = useState(null);
+    const [name, setName] = useState('Mario');
 
-    const handleDelete = (id: number) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-    }
-
-    useEffect(()=>{
-        console.log("use effect ran");
-        console.log(name)
-    },[name])
+    useEffect(() => {
+        fetch(' http://localhost:8000/blogs').then(
+            res => {
+                return res.json()
+            }
+        ).then((data) => {
+            console.log(data);
+            setBlogs(data);
+        })
+    }, [])
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
-            {/*<BlogList blogs={blogs.filter((b)=>b.author==='mario')} title="Mario's Blogs"/>*/}
-            <button onClick={()=>setName("Sandeera")}>Change Name</button>
-            <p>name: {name}</p>
+            { blogs && <BlogList blogs={blogs} title="All Blogs"/>}
         </div>
     );
 }
