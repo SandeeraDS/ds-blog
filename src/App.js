c14d66a7b77c287ef761656f70ca75a4c1a831af
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-05 05:38:52 +0530

    Step 15 - react route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./Navbar";
 import Home from "./Home";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import Create from "./Create";
 
 function App() {
     return (
@@ -9,8 +10,8 @@ function App() {
                 <Navbar/>
                 <div className="content">
                     <Routes>
-                        <Route path="/" element={<Home />}>
-                        </Route>
+                        <Route exact path="/" element={<Home />}/>
+                        <Route path="/create" element={<Create/>}/>
                     </Routes>
                 </div>
             </div>
