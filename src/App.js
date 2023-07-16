8627ef98ec962f39c9997db6ffd6c16b944c808f
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-16 14:51:23 +0530

    Step 22 - 404 page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 
 function App() {
     return (
@@ -11,9 +12,10 @@ function App() {
                 <Navbar/>
                 <div className="content">
                     <Routes>
-                        <Route exact path="/" element={<Home />}/>
+                        <Route exact path="/" element={<Home/>}/>
                         <Route path="/create" element={<Create/>}/>
                         <Route path="/blog/:id" element={<BlogDetails/>}/>
+                        <Route path="*" element={<NotFound/>}></Route>
                     </Routes>
                 </div>
             </div>
