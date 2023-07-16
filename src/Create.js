8739e1acf93598e7dfa989d87c24bc8ae72e0327
SandeeraJ <j.sandeera@gtngroup.com>
2023-07-16 13:35:54 +0530

    Step 19 - Making Post Request.

--- src/Create.js
+++ src/Create.js
@@ -1,37 +1,51 @@
 import {useState} from "react";
 
 const Create = () => {
-    const [title,setTitle] = useState(null);
+    const [title, setTitle] = useState(null);
     const [body, setBody] = useState(null);
-    const [author,setAuthor] = useState('mario')
+    const [author, setAuthor] = useState('mario');
+    const [isPending,setIsPending] = useState(false);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setIsPending(true);
+        const  blog = {title,body,author}
+        fetch('http://localhost:8000/blogs',{
+            method: 'POST',
+            headers:{"Content-Type":"application/json"},
+            body:JSON.stringify(blog)
+        }).then(()=>{
+            console.log('new blog added');
+            setIsPending(false);
+        });
+    }
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Blog title:</label>
                 <input
                     type="text"
                     required
                     value={title}
-                    onChange={(e)=>setTitle(e.target.value)}
+                    onChange={(e) => setTitle(e.target.value)}
                 />
                 <label>Blog Body:</label>
                 <textarea
-                required
-                value={body}
-                onChange={(e)=>setBody(e.target.value)}>
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}>
                 </textarea>
                 <label>Blog Author:</label>
                 <select
-                value={author}
-                onChange={(e)=>{setAuthor(e.target.value)}}>
+                    value={author}
+                    onChange={(e) => {
+                        setAuthor(e.target.value)
+                    }}>
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
-                <p>{title}</p>
-                <p>{body}</p>
-                <p>{author}</p>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button>Adding New...</button>}
             </form>
         </div>
     );
